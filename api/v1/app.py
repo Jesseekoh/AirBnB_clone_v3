2af5276762c5e7ea3274ab269d75c1ef0c836fa1
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2024-01-07 21:19:54 +0100

    fix some requirement failures from task 3

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,9 +10,11 @@ app = Flask(__name__)
 
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def cleanUp(exception):
     storage.close()
+
 
 if __name__ == '__main__':
     HBNB_API_HOST = getenv('HBNB_API_HOST', '0.0.0.0')
