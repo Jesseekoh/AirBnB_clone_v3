754705b31fe8b213d4e1c89181a03f0884c02b46
Edward-Odey <edwardodey04@gmail.com>
2024-01-09 14:24:34 +0100

    recent update

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -53,7 +53,7 @@ def delete_city(city_id):
     return {}
 
 
-@app_views.route('/states/<state_id>/cities', methods=['POST'],
+@app_views.route('states/<state_id>/cities', methods=['POST'],
                  strict_slashes=False)
 def create_city(state_id):
     """create a city with association to the
